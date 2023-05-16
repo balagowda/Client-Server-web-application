f88252f929557784c51545fa7508480cfed52031
balachandregowda <balagowda9483@gmail.com>
2023-05-16 23:18:57 +0530

    server initialization and front-end connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,45 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+mongoose
+  .connect("mongodb://localhost:27017/student")
+  .then(() => {
+    console.log("Connection success");
+  })
+  .catch(() => {
+    console.log("connection failed");
+  });
+
+  const newSchema = mongoose.Schema({
+    Name:{
+      type:String,
+      reuired:true
+    },
+    Mob:{
+      type:NumberInt(10),
+      length:10,
+      required:true
+    }
+  })
+
+  const collection = new mongoose.model("studentCollection",newSchema);
+  module.exports = collection;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Successful response.");
+app.use(express.urlencoded({extended:true}));
+
+app.get("/", cors(),(req, res) => {
+  
 });
 
-app.listen(3000,()=>{
-    console.log("server started");
-} );
+app.post('/',async(req,res)=>{
+    const {data} = req.body
+
+    await collection.insertMany([data]);
+})
+
+app.listen(3000, () => {
+  console.log("server started");
+});
